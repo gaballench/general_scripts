15b25806ecc191301d8ea7103b746e9b4abe6e4c
Gustavo A. Ballen <gaballench@gmail.com>
2017-05-09 13:33:14 -0500

    Correct the `except` argument in order to protect the function itself

--- rmExcept.R
+++ rmExcept.R
@@ -7,6 +7,6 @@
 #`except` = An atomic vector indicating the name(s) of the element(s) to preserve in the global environment.
 
 rmExcept <- function(except) {
-    rm(list = ls(envir = .GlobalEnv)[!(ls(envir = .GlobalEnv) %in% except)], 
+    rm(list = ls(envir = .GlobalEnv)[!(ls(envir = .GlobalEnv) %in% c(except, "rmExcept"))], 
         envir = .GlobalEnv)
 }
