ea0c7dfb26eb3b7db805d6aa26380670b0d16369
Gustavo A. Ballen <gaballench@gmail.com>
2017-05-09 13:30:52 -0500

    Create rmExcept.R

---
+++ rmExcept.R
@@ -0,0 +1,12 @@
+## Description
+#
+#Function for cleaning the global environment except those elements indicated in the `except` argument
+#
+## Arguments
+#
+#`except` = An atomic vector indicating the name(s) of the element(s) to preserve in the global environment.
+
+rmExcept <- function(except) {
+    rm(list = ls(envir = .GlobalEnv)[!(ls(envir = .GlobalEnv) %in% except)], 
+        envir = .GlobalEnv)
+}
