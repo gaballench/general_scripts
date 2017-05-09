ab48b18a3161cbbb9b7155bf5f9b511c623cb455
Gustavo A. Ballen <gaballench@gmail.com>
2017-05-09 13:44:50 -0500

    Add authorship

--- rmExcept.R
+++ rmExcept.R
@@ -5,6 +5,9 @@
 ## Arguments
 #
 #`except` = An atomic vector indicating the name(s) of the element(s) to preserve in the global environment.
+#
+## Author
+# Gustavo A. Ballen. 09-may2017
 
 rmExcept <- function(except) {
     except <- c(except, "rmExcept")
