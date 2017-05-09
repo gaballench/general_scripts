3a690489392c4b89db40a9b27d6e3a524d8ed8fa
Gustavo A. Ballen <gaballench@gmail.com>
2017-05-09 13:41:06 -0500

    Concat object name list before remotion

--- rmExcept.R
+++ rmExcept.R
@@ -7,6 +7,7 @@
 #`except` = An atomic vector indicating the name(s) of the element(s) to preserve in the global environment.
 
 rmExcept <- function(except) {
-    rm(list = ls(envir = .GlobalEnv)[!(ls(envir = .GlobalEnv) %in% c(except, "rmExcept"))], 
+    except <- c(except, "rmExcept")
+    rm(list = ls(envir = .GlobalEnv)[!(ls(envir = .GlobalEnv) %in% except)], 
         envir = .GlobalEnv)
 }
